ad0e65d004eb767db223d551b6117b4c19786d04
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-06-16 18:47:40 +0200

    fix h1 center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import styles from "./HomePage.module.css";
 const HomePage: NextPage = () => {
   return (
     <div className={`${styles.container} flex items-center justify-center h-screen`}>
-      <h1 className={`${styles.text} text-4xl text-white font-bold`}>Wybierz typ aplikacji!</h1>
+      <h1 className={`items-center text-4xl text-white font-bold`}>Wybierz typ aplikacji!</h1>
     </div>
   );
 };
